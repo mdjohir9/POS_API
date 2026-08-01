65c0dd1f1904a0573020fabc8dea976ea2991ef2
Johir Raihan <mdjohirullislam728@gmail.com>
2026-08-02 01:53:52 +0600

    Initial commit

--- obj/Debug/net9.0/POS-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/POS-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db2cd7b8cba7c643bac6880ff848e327d60290b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
