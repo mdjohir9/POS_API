09c543f6b9fec4cd5926109c3eb5df91108a46fd
Md Johirull Islam <76054704+mdjohir9@users.noreply.github.com>
2026-08-11 01:19:01 +0600

    CreateSales function add with hear dto

--- Migrations/20260810191526_pos_payment_method_add_on_sales_master.Designer.cs
+++ Migrations/20260810191526_pos_payment_method_add_on_sales_master.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using POS_API.Entities;
 
@@ -11,9 +12,11 @@ using POS_API.Entities;
 namespace POS_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260810191526_pos_payment_method_add_on_sales_master")]
+    partial class pos_payment_method_add_on_sales_master
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260810191526_pos_payment_method_add_on_sales_master.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class pos_payment_method_add_on_sales_master : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
