a2b1b769b6bd9722c2da0b2143602f29f5f613d8
Md Johirull Islam <76054704+mdjohir9@users.noreply.github.com>
2026-08-16 02:11:19 +0600

    test devlop

--- obj/Debug/net9.0/POS-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/POS-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5098ff3bca3051649f01a30164fd02f6e3cc241c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75bdd35608ed13cc97353773ae7ca5b74c0d56a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
